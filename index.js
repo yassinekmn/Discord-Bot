0b297697f9d7e8320322b14efc3d2037ddb037c2
yassinekmn <yassine.kammoun@esprit.tn>
2026-04-14 03:11:50 +0100

    try mweb client

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 const EXTRA_ARGS = [
   '--no-check-certificate',
   '--cookies', COOKIES,
-  '--extractor-args', 'youtube:player_client=ios',
+  '--extractor-args', 'youtube:player_client=mweb',
   '--force-ipv4',
   '--add-header', `User-Agent:${UA}`,
 ];
